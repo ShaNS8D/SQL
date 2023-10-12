720c9f2dd508a8602e4256ff283385c7e10d3750
Shalaevskiy Nikolay <nshalaevskiy@intraservices.ru>
2023-10-12 16:41:31 +0300

    change

--- query_python.py
+++ query_python.py
@@ -33,20 +33,27 @@ def add_client(conn, first_name, last_name, email, phones=None):
             cur.execute("""
             INSERT INTO Phones(client_id, phone) VALUES(%s, %s);
             """, (client_id, phones))
-            conn.commit()
 
 def add_phone(conn, client_id, phone):
     with conn.cursor() as cur:
         cur.execute("""
             INSERT INTO Phones(client_id, phone) VALUES(%s, %s);
             """, (client_id, phone))
-        conn.commit()
 
 def change_client(conn, client_id, first_name=None, last_name=None, email=None, phone=None):
-    with conn.cursor() as cur:        
-        cur.execute("""
-            UPDATE Client SET first_name=%s, last_name=%s, email=%s WHERE id=%s;
-            """, (first_name, last_name, email, client_id))
+    with conn.cursor() as cur:
+        if first_name:
+            cur.execute("""
+                UPDATE Client SET first_name=%s WHERE id=%s;
+                """, (first_name, client_id))
+        if last_name:
+            cur.execute("""
+                UPDATE Client SET last_name=%s WHERE id=%s;
+                """, (last_name, client_id))
+        if email:
+            cur.execute("""
+                UPDATE Client SET email=%s WHERE id=%s;
+                """, (email, client_id))
         cur.execute("""
             SELECT id FROM Phones WHERE client_id=%s ORDER BY id DESC LIMIT 1;
             """, (client_id, ))
@@ -54,14 +61,12 @@ def change_client(conn, client_id, first_name=None, last_name=None, email=None, 
         cur.execute("""
             UPDATE Phones SET phone=%s WHERE id=%s AND client_id=%s;
             """, (phone, phone_id, client_id))
-        conn.commit()
 
 def delete_phone(conn, client_id, phone):
     with conn.cursor() as cur:        
         cur.execute("""
             DELETE FROM phones WHERE client_id=%s AND phone=%s;
             """, (client_id, phone))
-        conn.commit()
 
 def delete_client(conn, client_id):
     with conn.cursor() as cur:
@@ -73,33 +78,41 @@ def delete_client(conn, client_id):
             """, (client_id,))
 
 def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
+    if first_name and last_name and email and phone:
+        query_str = "first_name=%s AND last_name=%s AND email=%s AND p.phone=%s;"
+    
+    
+
+
     with conn.cursor() as cur:
-        cur.execute("""
-            SELECT * FROM Client AS c JOIN Phones AS p ON c.id = p.client_id WHERE first_name=%s OR last_name=%s 
-            OR email=%s OR p.phone=%s;
-            """, (first_name, last_name, email, phone))
+        cur.execute("SELECT * FROM Client AS c JOIN Phones AS p ON c.id = p.client_id WHERE " + "first_name=%s OR last_name=%s OR email=%s OR p.phone=%s;",
+                     (first_name, last_name, email, phone))
         print(cur.fetchall())
 
+if __name__ == "__main__":
+    db = 'homework5'
+    username = 'postgres'
+    passw = '253553'
+    with psycopg2.connect(database=db, user=username, password=passw) as conn:
+        # create_table(conn)
+        # add_client(conn, 'Вася', 'Пузиков', 'puzik@mail.ru', '9215558465')
+        # add_client(conn, 'Толя', 'Куров', 'kirovt@mail.ru', '')
 
-db = 'homework5'
-username = 'postgres'
-passw = '253553'
-with psycopg2.connect(database=db, user=username, password=passw) as conn:
-    create_table(conn)
-    add_client(conn, 'Вася', 'Пузиков', 'puzik@mail.ru', '9215558465')
-    add_client(conn, 'Толя', 'Куров', 'kirovt@mail.ru', '')
+        # add_phone(conn, 1, '7897897891')
+        # add_phone(conn, 2, '1594876321')
 
-    add_phone(conn, 1, '7897897891')
-    add_phone(conn, 2, '1594876321')
+        # change_client(conn, 2, 'Толя', 'Киров', 'kirovt@mail.ru', '1234567895')
+        # change_client(conn, 2, first_name='Митя')
+        # change_client(conn, 2, last_name='Хомяков')
+        # change_client(conn, 2, email='kirovt@gmail.ru')
+        # change_client(conn, 2, phone='3216549873')
 
-    change_client(conn, 2, 'Толя', 'Киров', 'kirovt@mail.ru', '1234567895')
+        # delete_phone(conn, 1, '7897897891')
+        # delete_client(conn, 2)
 
-    delete_phone(conn, 1, '7897897891')
-    delete_client(conn, 2)
+        find_client(conn, last_name='Хомяков')
+        # find_client(conn, last_name='Пузиков')
+        # find_client(conn, email='puzik@mail.ru')
+        # find_client(conn, phone='9215558465')
 
-    find_client(conn, first_name='Вася')
-    find_client(conn, last_name='Пузиков')
-    find_client(conn, email='puzik@mail.ru')
-    find_client(conn, phone='9215558465')
-
-conn.close()
+    # conn.close()
